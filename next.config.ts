e59050e088e020d288d5df4b5a8b416008bb53e4
John Becerra <cardonabecerrajonathanmanuel@gmail.com>
2026-04-08 09:13:57 -0600

    fix: add assetPrefix for landing static assets

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
+  assetPrefix: isProd ? "https://cefin-landings-z9uk.vercel.app" : "",
   images: {
     dangerouslyAllowSVG: true,
     contentDispositionType: "attachment",
