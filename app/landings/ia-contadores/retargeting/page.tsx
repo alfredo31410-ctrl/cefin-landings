4c4e17fd23a8ed03d8572216fa16fd6126dccc55
John Becerra <cardonabecerrajonathanmanuel@gmail.com>
2026-04-21 14:02:11 -0600

    Cambio de pixel de IA a General

--- app/landings/ia-contadores/retargeting/page.tsx
+++ app/landings/ia-contadores/retargeting/page.tsx
@@ -9,7 +9,7 @@ declare global {
   }
 }
 
-const META_PIXEL_ID = "733425513099672";
+const META_PIXEL_ID = "1485908226564541";
 const PAYMENT_URL =
   "https://pay.hotmart.com/J105434552K?off=399ngaxa&checkoutMode=10&bid=1776798830168";
 
