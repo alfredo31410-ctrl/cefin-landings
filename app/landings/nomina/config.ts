0338a1842da7f68dfab8985a46a1d34908282785
JohnBecerra16 <160516556+JohnBecerra16@users.noreply.github.com>
2026-08-02 10:21:16 -0600

    Cambio al grupo 6 de nomima

--- app/landings/nomina/config.ts
+++ app/landings/nomina/config.ts
@@ -105,7 +105,7 @@ export const landingConfig = {
     enabled: true,
     whatsappRedirectPath: "/landings/nomina/unirse-whatsapp",
     whatsappEnabled: true,
-    whatsappGroupUrl: "https://chat.whatsapp.com/IemtfbpyV5o7PAF7ml2lfP?s=cl&p=a&ilr=1",
+    whatsappGroupUrl: "https://chat.whatsapp.com/K6FT4L0ipD8IQJM1ePJNsD?s=cl&p=a&ilr=1",
   },
 } as const;
 
